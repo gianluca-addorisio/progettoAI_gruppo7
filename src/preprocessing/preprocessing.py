25a7cc5fae2725924129b570c5dd026ad7ada7bc
erikaoliveri <erikaoliveri92@gmail.com>
2025-12-23 14:59:04 +0100

    Definita struttura e documentazione della classe DataPreprocessor

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,1 +1,23 @@
-# TODO: implement
+import pandas as pd
+import numpy as np
+
+class DataPreprocessor:
+
+    def __init__(self, feature_cols: list[str], label_col: str):
+        self.feature_cols = feature_cols
+        self.label_col = label_col
+        self.fitted = False   #impedisce l'uso di transform() prima di fit()
+
+    def  fit(selfself, df):
+        #apprende i parametri di preprocessing usando esclusivamente il training set
+        pass
+
+    def transform(self, df):
+        #applica le trasformazioni apprese nel fit a training set o test set
+        pass
+
+    def fit_transform(self, df):
+        # combina fit() e transform() sul training set
+        pass
+
+
